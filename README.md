6488d4592f108875d244b1dbbfa63d78d4aa4298
Logan Hood <hoodlma@gmail.com>
2023-01-21 14:22:30 -0500

    Initial implementation

---
+++ README.md
@@ -0,0 +1,37 @@
+This is a simple bash script for offline reading of Hacker News
+(news.ycombinator.com). It downloads the front page (the top 30 items)
+along with their comment threads for offline reading.
+
+```
+bash hn_offline.sh
+```
+
+External links are not archived; these are still offline links.
+
+It does not archive other features of the website, like:
+* User pages
+* The 'from' pages to discover other articles from a certain source
+* Anything after page 1
+
+It also doesn't download any non-HTML assets like CSS or Javascript. This
+means some functions on the website don't work; for example, you can't
+collapse threads or hide individual posts.
+
+# What's the purpose of this?
+
+I use it to use Hacker News like a "newspaper" instead of a constantly-
+changing page that I try to check every day or even multiple times a day to
+see if there's something new.
+
+I run a weekly cronjob that refreshes an offline copy on a local web server.
+Then, I can point my browser at that local copy instead of at
+news.ycombinator.com.
+
+# Is this allowed?
+
+Before using any crawling tool you should consult the website's robots.txt.
+
+HN's [robots.txt](https://news.ycombinator.com/robots.txt), as of
+2023-01-21, does not forbid crawling the main page or `/item?` paths. We
+use a default sleep of 31 seconds between downloads to comply with the
+`Crawl-delay` of 30 seconds.
---
+++ hn_offline.sh
@@ -0,0 +1,84 @@
+#! /usr/bin/bash
+set -eu
+set -o pipefail
+set -o posix
+
+# --------------------------------------------------------------------
+# Usage: download "news.ycombinator.com" "./out/index.html"
+download() {
+  local url=$1
+  local download_outfile=$2
+
+
+  printf "Downloading %s to %s\n" "${url}" "${download_outfile}"
+  wget --quiet "$url" -O "${download_outfile}"
+
+  # respect HN's robots.txt directive 'Crawl-delay: 30'
+  local sleep_seconds=31
+  printf "Waiting %s seconds between downloads\n" "${sleep_seconds}"
+  sleep "${sleep_seconds}"
+}
+
+# --------------------------------------------------------------------
+# Comment links on HN follow the format like "item?id=34466855"
+# where id is a parameter to a backend webserver. Since we're just
+# saving these as static HTML, and it's a pain to work with unix
+# files with question marks in them, I'm just stripping out
+# the '?' from all comment URLs.
+sanitize_comment_links() {
+  sed "s/item?id/itemid/g" -i $1
+}
+
+# --------------------------------------------------------------------
+# obligatory before we go further,
+# https://stackoverflow.com/a/1732454
+#
+# "we shall surely both die if you parse html with
+# regular expressions," said the Frog.
+# "lol" said the Scorpion, "lmao"
+
+
+# --------------------------------------------------------------------
+# Extract all 'comment' links from the hacker news frontpage
+# and writes them to ./tmp/comments.txt
+# 
+# typically: comment_links ./out/index.html
+extract_comment_links() {
+  local html_file=$1
+
+  local comment_urls
+
+  # the first regex will give you a list of things like
+  #   <a href="item?id=34466855">46&nbsp;comments</a>
+  # then the second strips out just the first double-quoted segment:
+  #   "item?id=34466855"
+  # and finally a sed command to remove the double quotes:
+  #   item?id=34466855
+
+  comment_urls=$(\
+    grep -E -o "<a href=\"[^<>]+\">[^<>]+comments</a>" "$html_file" \
+    | grep -E -o "\".+\"" \
+    | sed 's/\"//g')
+
+  echo ${comment_urls} > ./tmp/comments.txt
+}
+
+
+# --------------------------------------------------------------------
+main() {
+  printf "Clearing ./tmp and ./out directories\n"
+  rm ./tmp/* || true
+  rm ./out/* || true
+
+  download "news.ycombinator.com" "./out/index.html"
+  extract_comment_links "./out/index.html"
+  sanitize_comment_links "./out/index.html"
+
+  for comment_url in $(cat "./tmp/comments.txt"); do
+    local dest_file
+    dest_file=$(echo $comment_url | sed "s/item?id/itemid/g")
+    download "news.ycombinator.com/${comment_url}" "./out/${dest_file}"
+  done
+}
+
+main
